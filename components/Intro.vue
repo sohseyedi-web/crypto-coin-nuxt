6a57af4a2e10f0cbd19072e27a616f5de3829a24
sohseyedi <sohseyedi@gmail.com>
2024-11-28 18:57:50 +0330

    feat: create hero section with navigation links

---
+++ components/Intro.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="flex items-center justify-center flex-col pt-14">
+        <h3 class="text-5xl font-bold text-[#D91656]">Crypto Nuxt</h3>
+        <div class="flex items-center gap-5 text-zinc-100 my-5">
+            <ButtonLink to="/coins" title="All Coins"/>
+            <ButtonLink to="/following" title="Following"/>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import ButtonLink from './button/ButtonLink.vue';
+</script>
