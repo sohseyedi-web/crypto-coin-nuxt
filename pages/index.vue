d6584a856a0bbe38081b2d819f38b7c55a0365db
sohseyedi <sohseyedi@gmail.com>
2024-11-28 18:36:32 +0330

    feat:Add components to the main template for better modularity and content organization

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
-        salam
-    </div>
+    <Intro/>
+    <TopTenCoins/>
+    <SwiperCoins/>
 </template>
 
 <script setup lang="ts">
+import Intro from '~/components/Intro.vue';
+import TopTenCoins from '~/components/TopTenCoins.vue';
+import SwiperCoins from '~/components/SwiperCoins.vue';
+
 
 </script>
