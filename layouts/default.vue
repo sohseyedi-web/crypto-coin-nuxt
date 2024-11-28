52f93117d64f14bd57a0a8155ab9a36e709859fd
sohseyedi <sohseyedi@gmail.com>
2024-11-28 18:58:51 +0330

    feat:create default and set header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
-    <slot/>
+    <Header/>
+    <main class="p-3 max-w-7xl mx-auto">
+        <slot/>
+    </main>
 </template>
 
 <script setup lang="ts">
+import Header from '~/components/Header.vue';
+
 
 
 </script>
