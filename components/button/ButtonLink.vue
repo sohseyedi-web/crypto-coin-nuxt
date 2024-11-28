1fbedc7b87666459ef2f80b19608624b874245e7
sohseyedi <sohseyedi@gmail.com>
2024-11-28 18:38:57 +0330

    feat:reusable button component with customizable link

---
+++ components/button/ButtonLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <NuxtLink :to="to" >
+        <button class="hover:bg-rose-700 transition-all duration-300 w-[210px] h-[50px] font-semibold rounded-xl flex items-center justify-center bg-[#d91656] text-xl">
+            {{title}}
+        </button>
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const {to,title} = defineProps({
+    to:{
+        type : String,
+        required:true
+    },
+    title:{
+        type : String,
+        required:true
+    },
+})
+</script>
