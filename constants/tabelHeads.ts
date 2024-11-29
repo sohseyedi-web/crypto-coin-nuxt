f8fe6a8f8a63487dcc33a2c3f2bc34956cd48e0d
sohseyedi <sohseyedi@gmail.com>
2024-11-29 18:15:23 +0330

    feat:write coin table header data

---
+++ constants/tabelHeads.ts
@@ -0,0 +1,27 @@
+export const coinTableHeads = [
+  {
+    id: 1,
+    label: "#",
+  },
+  {
+    id: 2,
+    label: "Name",
+  },
+  {
+    id: 3,
+    label: "Price",
+  },
+
+  {
+    id: 4,
+    label: "Market Price",
+  },
+  {
+    id: 5,
+    label: "24h Changes",
+  },
+  {
+    id: 6,
+    label: "Details",
+  },
+]
